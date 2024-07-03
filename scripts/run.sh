d3a1c23cbe5b204cf04f8f6c09eea78b0690a23e
Le Hoang Bao <72609761+baohl00@users.noreply.github.com>
2024-07-03 15:24:46 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,5 @@
 #MODEL="./Vistral-7B-Chat"
 #MODEL="/home/support/llm/Meta-Llama-3-8B-Instruct"
-#MODEL="vilm/vinallama-2.7b-chat"
-#MODEL="uonlp/Vistral-7B-Chat-gguf"
 #MODEL="NousResearch/Llama-2-7b-chat-hf"
 #MODEL="chillies/vistral-legal-chat-q4"
 MODEL="chillies/vinallama-legal-chat"
